dfa64b293b10ec6dc6c0538bcdc45d2db07109dc
Mor <alnetworking@hotmail.com>
2020-10-01 08:46:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ bot.on('ready', function() {
 });
 ///////
 function frasespregunta() {
-  var rand = ['Si', 'No', 'Ni idea', 'Mejor que te responda otro que yo no tengo ni puta idea xd', 'Hay articulo ya en Vandal por si lo quieres mirar', 'Desde 2018 como mod y lo único que hace este chico es preguntar', 'Hola Allegrian :3', 'Si yo te contara, madre mía',
+  var rand = ['Si', 'No', 'Ni idea', 'Mejor que te responda otro que yo no tengo ni puta idea xd', 'Hay articulo ya en Vandal sobre eso, por si lo quieres mirar', 'Desde 2018 como mod y lo único que hace este chico es preguntar', 'Hola Allegrian :3', 'Si yo te contara, madre mía',
               '¿Otra pregunta? joder Alle, ya te vale', 'Google está a un click de distancia', 'Tengo la respuesta en la punta del hocico', '¯|_(ツ)_|¯', 'Con diferencia el peor mod del server, no toma decisiones, solo pregunta cosas, administración de pacotilla',
-              'El server no es tu wikipedia personal, a informarse por ahí', 'Lo de preguntar todo el rato lo haces a posta, verdad? xd'];
+              'El server no es tu wikipedia personal, a informarse por ahí', 'Lo de preguntar todo el rato lo haces a posta, verdad? xd', '?', ' pa k kieres saber eso jaja saludos'];
 
   return rand[Math.floor(Math.random()*rand.length)];
 }
