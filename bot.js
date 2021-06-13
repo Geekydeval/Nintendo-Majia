0f46045f547b70e24faf121fe4e42f85cfc528ff
Mor <alnetworking@hotmail.com>
2021-06-13 15:56:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,25 @@ bot.on('ready', function() {
 });
 ///////
 function Piscatella() {
-  var rand = ['Piscatella, Gilipollas', 'B-O-C-A-C-H-A-N-C-L-A-S'];
+  var rand = ['Piscatella, Gilipollas', 'B-O-C-A-C-H-A-N-C-L-A-S', 'madre mía, ¿TE QUIERES CALLAR YA?', 'Piscatella, tienes menos criterio que Lanir', 'Mi sentido analista dice que estás en el puesto #1 de tarado'];
+
+  return rand[Math.floor(Math.random()*rand.length)];
+}
+
+function Geoff() {
+  var rand = ['A ti quien te ha dado vela en este entierro', 'Joder Geoff, peor que un niño chico, QUE TIENES PELOS EN LOS HUEVOS, HOMBRE', 'Si no te hacemos casito no eres feliz, eh?', 'Madre mía, te dejamos de hacer caso 5 minutos y tienes que llamar la atención, tela.'];
+
+  return rand[Math.floor(Math.random()*rand.length)];
+}
+
+function Nibel() {
+  var rand = ['Deja de robar contenido, cateto', '¿Has acreditado ya a los autores correspondientes?¿no? vaya, que inesperado', 'Nibel, Subnormal!', 'Si algo tiene claro este bot es que Nibel es un pícaro ladronzuelo', 'Germano tenías que ser xd'];
 
   return rand[Math.floor(Math.random()*rand.length)];
 }
 
 function frasesban() {
-  var rand = ['NO HAY COJONES A BANEAR AL ADMIN', 'Vamos a ver, algarroba, Mor es el admin, ¿como coño lo vas a banear?', 'A ti si te vamos a banear, pero de un hostiazo' ];
+  var rand = ['A ti si te vamos a banear, pero de un hostiazo', 'Cierra el pico o quien acabará baneado eres tu, algarroba', 'HOSTIA MANOLO, ENHORABUENA, TIENES TODAS LAS PAPELETAS PARA SER BANEADO' ];
 
   return rand[Math.floor(Math.random()*rand.length)];
 }
@@ -23,6 +35,12 @@ function frasesban() {
 bot.on('message', function(message) {
   if ((message.content.match(new RegExp('https://twitter.com/MatPiscatella/status', 'g')))){
       message.channel.send((Piscatella()));
+    }
+  else if ((message.content.match(new RegExp('https://twitter.com/geoffkeighley/status', 'g')))){
+      message.channel.send((Geoff()));
+    }
+   else if ((message.content.match(new RegExp('https://twitter.com/Nibellion/status/', 'g')))){
+      message.channel.send((Nibel()));
     }
   else if (message.content.match(':banmor:')) {
     message.channel.send((frasesban()));
