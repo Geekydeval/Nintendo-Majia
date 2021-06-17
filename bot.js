a4e32d8d7d4538ad27119710ee2f540a80ff9fbc
Mor <alnetworking@hotmail.com>
2021-06-17 23:35:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,8 @@ function Piscatella() {
 }
 
 function Chaos() {
-  var rand = ['chaos'];
+  var rand = ['No se dice caos, se dice _*Chaos*_', 'https://images-ext-1.discordapp.net/external/th6XCiXaaM9qscxOrMFS1CHNlYQdeIwfr_hA4h41k5I/https/pbs.twimg.com/media/E4CkWZAWUAA81c4.jpg', 'Para caos el de este chat',
+               'Im here to kill chaos, bitches'];
 
   return rand[Math.floor(Math.random()*rand.length)];
 }
